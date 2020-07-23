47f9dcaec1b7606fecf82287a3cebbe7789392e5
RaaZo <koprowicz.mateusz@gmail.com>
2020-07-23 19:37:23 +0200

    Updates

---
+++ precache-manifest.cc47d38a8de50332f2418a0cf4a2cefb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510f22cce004cfa121e471a3e2821a37",
+    "url": "/Hearthstone-App/index.html"
+  },
+  {
+    "revision": "8fdf4b0d8c772cc7e7fe",
+    "url": "/Hearthstone-App/static/css/main.0348cdbe.chunk.css"
+  },
+  {
+    "revision": "b5224cff67d6955cb76f",
+    "url": "/Hearthstone-App/static/js/2.63ec8691.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hearthstone-App/static/js/2.63ec8691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdf4b0d8c772cc7e7fe",
+    "url": "/Hearthstone-App/static/js/main.d058e95d.chunk.js"
+  },
+  {
+    "revision": "1e01eb3d3e8addbe55b9",
+    "url": "/Hearthstone-App/static/js/runtime-main.9ba55d64.js"
+  },
+  {
+    "revision": "6343d7fbcec4756f3c1b6b2f71c0a6ac",
+    "url": "/Hearthstone-App/static/media/CardReversesHero.6343d7fb.jpg"
+  },
+  {
+    "revision": "135ad2acf472020c4563fad203a4e045",
+    "url": "/Hearthstone-App/static/media/ExpansionsHero.135ad2ac.jpg"
+  },
+  {
+    "revision": "16e88446225c27c8e41f4a79512f4d07",
+    "url": "/Hearthstone-App/static/media/HomeBacks.16e88446.jpg"
+  },
+  {
+    "revision": "2dcfb1eba2eb03fdb0490f3d4018ae65",
+    "url": "/Hearthstone-App/static/media/HomeCards.2dcfb1eb.jpg"
+  },
+  {
+    "revision": "116bb1dd275847c0b1da1ad6c7831e5f",
+    "url": "/Hearthstone-App/static/media/HomeHero.116bb1dd.jpg"
+  },
+  {
+    "revision": "66474fb7cbcfce46dafc3c17baede83b",
+    "url": "/Hearthstone-App/static/media/LandingHero.66474fb7.jpg"
+  },
+  {
+    "revision": "db1ab9ceda6239327bfdcb28e6f7fc86",
+    "url": "/Hearthstone-App/static/media/QualityOfCardsHero.db1ab9ce.jpg"
+  },
+  {
+    "revision": "006fd0dcacc7d49a1fab98e272d23e10",
+    "url": "/Hearthstone-App/static/media/loadingIndicator.006fd0dc.svg"
+  },
+  {
+    "revision": "4056e676a3bba92af647cec40521ba6e",
+    "url": "/Hearthstone-App/static/media/typesOfCardsHero.4056e676.jpg"
+  }
+]);
